ffd098032a6264eebc67b577a7cbc378c0296cf4
ifazu80 <163073324+ifazu80@users.noreply.github.com>
2024-03-24 05:10:29 -0400

    yeooo main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "RHEL" {
+resource "aws_instance" "RHEL1" {
   count         = 1
   ami           = "ami-0fe630eb857a6ec83"
   instance_type = "t2.medium"
@@ -10,6 +10,7 @@ resource "aws_instance" "RHEL" {
   tags = {
     Name = "RHEL"
   }
+}
 resource "aws_instance" "Ubuntu1" {
   count         = 1
   ami           = "ami-080e1f13689e07408"
