4177c5d97be7f9acd3df33d908d1c47d0cf18525
ifazu80 <163073324+ifazu80@users.noreply.github.com>
2024-03-24 04:33:04 -0400

    Update10 main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+region" {
+default = "us-east-1"
+description = "aws region"
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_instance" "RHEL" {
