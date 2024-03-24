2712c60f664a32791c50f35390220079e4d7d778
ifazu80 <163073324+ifazu80@users.noreply.github.com>
2024-03-24 04:06:28 -0400

    Update7 main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,10 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
+provider "AWS" {
   region = "us-east-1"
 }
 
-# Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
